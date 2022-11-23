f9d17f68034120097aeabf633c616a964b6e6389
powcoder <powcoder@163.com>
2022-11-23 12:10:43 +0800

    CS494 lab1 FAT

---
+++ jdisk.h
@@ -0,0 +1,22 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#ifndef _JDISK_
+#define _JDISK_
+
+#define JDISK_SECTOR_SIZE (1024)
+#define JDISK_DELAY (1)
+
+void *jdisk_create(char *fn, unsigned long size);
+void *jdisk_attach(char *fn);
+int jdisk_unattach(void *jd);
+
+int jdisk_read(void *jd, unsigned int lba, void *buf);
+int jdisk_write(void *jd, unsigned int lba, void *buf);
+
+unsigned long jdisk_size(void *jd);
+long jdisk_reads(void *jd);
+long jdisk_writes(void *jd);
+
+#endif
